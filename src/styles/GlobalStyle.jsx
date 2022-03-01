d6134a823ef771646f87bb401080615a0d85b91e
Jossany Moura <mjossany@gmail.com>
2022-03-01 12:13:43 -0300

    Sets up global styles configurations

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,15 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root{
+    --primary-color: #007bff;
+    --secondary-color: #6c757d;
+    --background-dark-color: #10121A;
+    --border-color: #2e344e;
+    --background-light-color: #F1F1F1;
+    --white-color: #FFF;
+    --font-light-color: #a4acc4;
+    --font-dark-color: #313131;
+    --font-dark-color-2: #151515;
+  }
+
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-size: 1.2rem;
+    list-style: none;
+    text-decoration: none;
   }
 
   body {
-    background-color: #333;
+    background-color: var(--background-dark-color);
   }
 `;
 
