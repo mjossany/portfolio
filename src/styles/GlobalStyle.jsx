49edea2e96e8b2c8f45e7129c86a748db22c4def
Jossany Moura <mjossany@gmail.com>
2022-03-01 18:09:45 -0300

    Revert "Sidebar"

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -20,19 +20,10 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     list-style: none;
     text-decoration: none;
-    font-family: 'Nunito', sans-serif;
-    font-size: 1.2rem;
   }
 
   body {
     background-color: var(--background-dark-color);
-    color: var(--font-light-color);
-  }
-
-  a{
-    font-family: inherit;
-    color: inherit;
-    font-size: inherit;
   }
 `;
 
