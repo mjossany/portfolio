afdb6b9f1bf40cbb90270c802683d8d76eb93040
Jossany Moura <mjossany@gmail.com>
2022-03-03 16:33:52 -0300

    Sets up routes on app for each page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import MainContent from './components/MainContent';
+import AboutPage from './pages/AboutPage';
+import ResumePage from './pages/ResumePage';
+import PortfolioPage from './pages/PortfolioPage';
+import ContactPage from './pages/ContactPage';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div>
       <Sidebar />
-      <MainContent />
+      <Routes>
+        <Route exact path="/" element={<MainContent />} />
+        <Route exact path="/about" element={<AboutPage />} />
+        <Route exact path="/resume" element={<ResumePage />} />
+        <Route exact path="/portfolio" element={<PortfolioPage />} />
+        <Route exact path="/contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
