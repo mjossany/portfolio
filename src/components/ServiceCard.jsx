9d567952c9c7b818933a077c855fdc0a5c89b64f
Jossany Moura <mjossany@gmail.com>
2022-03-04 15:57:49 -0300

    Creates ServiceCard component

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ServiceCard() {
+  return (
+    <div>ServiceCard</div>
+  );
+}
+
+export default ServiceCard;
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
 import { InnerLayout } from '../styles/Layouts';
+import Title from './Title';
+import ServiceCard from './ServiceCard';
+import design from '../images/design.svg';
+import intelligence from '../images/intelligence.svg';
+import gamedev from '../images/game-dev.svg';
 
 function ServicesSection() {
   return (
     <InnerLayout>
-      <ServicesSectionStyled />
+      <ServicesSectionStyled>
+        <Title title="Services" span="Services" />
+        <div className="services">
+          <ServiceCard
+            image={design}
+            title="Web Design"
+            paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Omnis iure culpa, ducimus corporis non exercitationem
+              officiis nihil deserunt quaerat!"
+          />
+        </div>
+        <div className="services">
+          <ServiceCard
+            image={intelligence}
+            title="Artificial Intelligence"
+            paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Omnis iure culpa, ducimus corporis non exercitationem
+              officiis nihil deserunt quaerat!"
+          />
+        </div>
+        <div className="services">
+          <ServiceCard
+            image={gamedev}
+            title="Game Development"
+            paragraph="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Omnis iure culpa, ducimus corporis non exercitationem
+              officiis nihil deserunt quaerat!"
+          />
+        </div>
+      </ServicesSectionStyled>
     </InnerLayout>
   );
 }
