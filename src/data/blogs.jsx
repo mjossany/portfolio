fd017ee18b351d8cf27a0cfc3046fbee90bb6fa1
Jossany Moura <mjossany@gmail.com>
2022-03-05 12:33:20 -0300

    Creates the data that will be used to create and style the blogs page

---
+++ src/data/blogs.jsx
@@ -0,0 +1,36 @@
+import {
+  blog1, blog2, blog3, blog4,
+} from '../images/blogs/blog1.png';
+
+const blogs = [
+  {
+    id: 1,
+    title: 'How to create a Blog',
+    date: '01',
+    month: 'March',
+    image: blog1,
+  },
+  {
+    id: 2,
+    title: 'How to create a Blog',
+    date: '01',
+    month: 'March',
+    image: blog2,
+  },
+  {
+    id: 3,
+    title: 'How to create a Blog',
+    date: '01',
+    month: 'March',
+    image: blog3,
+  },
+  {
+    id: 4,
+    title: 'How to create a Blog',
+    date: '01',
+    month: 'March',
+    image: blog4,
+  },
+];
+
+export default blogs;
