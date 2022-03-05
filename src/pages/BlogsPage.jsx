539ebda5d48f2917d79ab37c740b0776dd5b1fac
Jossany Moura <mjossany@gmail.com>
2022-03-05 12:22:26 -0300

    Creates the BlogsPage and the route to BlogsPage

---
+++ src/pages/BlogsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function BlogsPage() {
+  return (
+    <BlogsPageStyled>BlogsPage</BlogsPageStyled>
+  );
+}
+
+const BlogsPageStyled = styled.div``;
+
+export default BlogsPage;
