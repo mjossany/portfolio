68adfa5e0b1d34b4ade9e3c8fd8157362b986891
Jossany Moura <mjossany@gmail.com>
2022-03-06 17:07:47 -0300

    Add stylization to the Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -54,6 +54,69 @@ const MenuStyled = styled.div`
       }
       ul {
         display: none;
+        transform: translateY(-100px);
+        transition: all .4s ease-in-out;
+      }
+      .portfolio-image {
+        &::before {
+          content: "";
+          position: absolute;
+          left: 2%;
+          top: 4%;
+          height: 0;
+          width: 0;
+          transition: all .4s ease-in-out;
+        }
+      }
+      .portfolio-image:hover {
+        ul {
+          transform: translateY(0);
+          display: block;
+          position: absolute;
+          left: 50%;
+          top: 40%;
+          transform: translate(-50%, -50%);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          transition: all .4s ease-in-out;
+          li {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: var(--border-color);
+            width: 3rem;
+            height: 3rem;
+            padding: 1rem;
+            border-radius: 50%;
+            margin: 0 .5rem;
+            transition: all .4s ease-in-out;
+            &:hover {
+              background-color: var(--primary-color);
+            }
+            a {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+            }
+          }
+          li:hover {
+            svg {
+              color: var(--white-color);
+            }
+          }
+          svg {
+            font-size: 2rem;
+          }
+        }
+        &::before {
+          height: calc(100% - 32%);
+          width: calc(100% - 4%);
+          background-color: white;
+          opacity: 0.9;
+          transform-origin: left;
+          transition: all .4s ease-in-out;
+        }
       }
     }
   }
