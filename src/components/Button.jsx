9b8b250d18a1d75042647be474de3b004dc51b13
Jossany Moura <mjossany@gmail.com>
2022-03-06 16:27:13 -0300

    Add initial stylization for the buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,49 @@
 import { arrayOf, func } from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
 function Button({ filter, button }) {
   return (
-    <div>
+    <ButtonsStyled>
       {
         button.map((btn) => (
-          <button type="button" key={btn} onClick={() => filter(btn)}>
+          <ButtonStyled type="button" key={btn} onClick={() => filter(btn)}>
             {btn}
-          </button>
+          </ButtonStyled>
         ))
       }
-    </div>
+    </ButtonsStyled>
   );
 }
+
+const ButtonStyled = styled.button`
+  outline: none;
+  border: none;
+  background-color: var(--background-light-color-2);
+  padding: .4rem 2rem;
+  font-size: inherit;
+  color: var(--white-color);
+  cursor: pointer;
+  transition: all .4s ease-in-out;
+  &:active, &:focus {
+    background-color: var(--primary-color);
+  }
+  &:hover {
+    background-color: var(--primary-color);
+  }
+  &:not(:last-child) {
+    margin-right: .6rem;
+  }
+`;
+
+const ButtonsStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 70%;
+  margin: 2.4rem auto;
+`;
 
 Button.propTypes = {
   filter: func.isRequired,
