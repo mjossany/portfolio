9514494dc79a4dbe5a6abb7e6a9ffe14776abcd9
Jossany Moura <mjossany@gmail.com>
2022-03-05 22:20:33 -0300

    Minor fixes on ResumePage layout

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,49 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
-import { InnerLayout, MainLayout } from '../styles/Layouts';
+import { InnerLayout } from '../styles/Layouts';
 import ProgressBar from './ProgressBar';
 import Title from './Title';
 
 function Skills() {
   return (
     <SkillsStyled>
-      <MainLayout>
-        <Title title="Resume" span="Resume" />
-        <InnerLayout>
-          <div className="skills">
-            <ProgressBar
-              title="HTML5"
-              width="50%"
-              text="50%"
-            />
-            <ProgressBar
-              title="CSS3"
-              width="50%"
-              text="50%"
-            />
-            <ProgressBar
-              title="JAVASCRIPT"
-              width="50%"
-              text="50%"
-            />
-            <ProgressBar
-              title="REACT JS"
-              width="50%"
-              text="50%"
-            />
-            <ProgressBar
-              title="MYSQL"
-              width="50%"
-              text="50%"
-            />
-            <ProgressBar
-              title="PYTHON"
-              width="50%"
-              text="50%"
-            />
-          </div>
-        </InnerLayout>
-      </MainLayout>
+      <Title title="My Skills" span="My Skills" />
+      <InnerLayout>
+        <div className="skills">
+          <ProgressBar
+            title="HTML5"
+            width="50%"
+            text="50%"
+          />
+          <ProgressBar
+            title="CSS3"
+            width="50%"
+            text="50%"
+          />
+          <ProgressBar
+            title="JAVASCRIPT"
+            width="50%"
+            text="50%"
+          />
+          <ProgressBar
+            title="REACT JS"
+            width="50%"
+            text="50%"
+          />
+          <ProgressBar
+            title="MYSQL"
+            width="50%"
+            text="50%"
+          />
+          <ProgressBar
+            title="PYTHON"
+            width="50%"
+            text="50%"
+          />
+        </div>
+      </InnerLayout>
     </SkillsStyled>
   );
 }
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import Skills from '../components/Skills';
+import { MainLayout } from '../styles/Layouts';
 
 function ResumePage() {
   return (
-    <ResumePageStyled>
+    <MainLayout>
       <Skills />
-    </ResumePageStyled>
+    </MainLayout>
   );
 }
-
-const ResumePageStyled = styled.div`
-
-`;
 
 export default ResumePage;
