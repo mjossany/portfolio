7820975d6b20f62633559c88135607e743d76500
Jossany Moura <mjossany@gmail.com>
2022-03-07 20:27:39 -0300

    Creates media query for Skills components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -52,6 +52,9 @@ const SkillsStyled = styled.section`
     grid-template-columns: repeat(2, 1fr);
     grid-row-gap: 2rem;
     grid-column-gap: 3rem;
+    @media screen and (max-width: 700px) {
+      grid-template-columns: repeat(1, 1fr);
+    }
   }
 `;
 
