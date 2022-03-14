6fd25c4fa3377020fdace851cffd9e185ebf6abe
Jossany Moura <mjossany@gmail.com>
2022-03-14 10:58:51 -0300

    Refactors the HamburguerIcon component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LightModeSwitch from './components/LightModeSwitch';
 import MainContent from './components/MainContent';
 import Sidebar from './components/Sidebar/Sidebar';
-import HamburguerIcon from './components/HamburguerIcon';
+import HamburguerIcon from './components/HamburguerIcon/HamburguerIcon';
 
 function App() {
   return (
---
+++ src/components/HamburguerIcon/HamburguerIcon.jsx
@@ -0,0 +1,21 @@
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import React, { useContext } from 'react';
+import HamburguerIconContainer from './Styled';
+import { PortfolioContext } from '../../context/portfolio';
+
+function HamburguerIcon() {
+  const {
+    navToggle,
+    setNavToggle,
+  } = useContext(PortfolioContext);
+  return (
+    <HamburguerIconContainer>
+      <IconButton onClick={() => setNavToggle(!navToggle)}>
+        <MenuIcon className="hamburguer-menu" />
+      </IconButton>
+    </HamburguerIconContainer>
+  );
+}
+
+export default HamburguerIcon;
