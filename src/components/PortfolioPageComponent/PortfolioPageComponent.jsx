f232eb179e3cafd2bdce5664533632be3a7d6d08
Jossany Moura <mjossany@gmail.com>
2022-03-17 18:42:58 -0300

    Really finishes PortfolioPage refactoring

--- src/components/PortfolioPageComponent/PortfolioPageComponent.jsx
+++ src/components/PortfolioPageComponent/PortfolioPageComponent.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InnerLayout, MainLayout } from '../../styles/Layouts';
 import FilterButtons from '../FilterButtons/FilterButtons';
 import Title from '../Title';
+import portfolio from '../../data/portfolio';
+import PortfolioProjects from '../PortfolioProjects/PortfolioProjects';
+
+const allCategories = ['All', ...new Set(portfolio.map((item) => item.category))];
 
 function PortfolioPageComponent() {
+  const [projects, setProjects] = useState(portfolio);
+  const [categories] = useState(allCategories);
+
+  const filter = (btn) => {
+    if (btn === 'All') {
+      setProjects(portfolio);
+      return;
+    }
+    const filteredData = portfolio.filter((item) => item.category === btn);
+    setProjects(filteredData);
+  };
+
   return (
     <MainLayout>
       <Title title="Portfolio" span="Portfolio" />
       <InnerLayout>
-        <FilterButtons />
-        
+        <FilterButtons filterByCategory={filter} buttonsCategories={categories} />
+        <PortfolioProjects projectsToDisplay={projects} />
       </InnerLayout>
     </MainLayout>
   );
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
-import Title from '../components/Title';
-import { InnerLayout, MainLayout } from '../styles/Layouts';
-import portfolio from '../data/portfolio';
-import PortfolioProjects from '../components/PortfolioProjects/PortfolioProjects';
-import FilterButtons from '../components/FilterButtons/FilterButtons';
-
-const allCategories = ['All', ...new Set(portfolio.map((item) => item.category))];
+import React from 'react';
+import PortfolioPageComponent from '../components/PortfolioPageComponent/PortfolioPageComponent';
 
 function PortfolioPage() {
-  const [projects, setProjects] = useState(portfolio);
-  const [categories] = useState(allCategories);
-
-  const filter = (btn) => {
-    if (btn === 'All') {
-      setProjects(portfolio);
-      return;
-    }
-    const filteredData = portfolio.filter((item) => item.category === btn);
-    setProjects(filteredData);
-  };
-
   return (
-    <MainLayout>
-      <Title title="Portfolio" span="Portfolio" />
-      <InnerLayout>
-        <FilterButtons filterByCategory={filter} buttonsCategories={categories} />
-        <PortfolioProjects projectsToDisplay={projects} />
-      </InnerLayout>
-    </MainLayout>
+    <PortfolioPageComponent />
   );
 }
 
