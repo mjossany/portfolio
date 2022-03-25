c65103d2067620c2e44de9d660880db3fc74efca
Jossany Moura <mjossany@gmail.com>
2022-03-25 10:47:15 -0300

    Removes unnecessary images

--- src/data/blogs.jsx
+++ src/data/blogs.jsx
@@ -1,7 +1,4 @@
 import blog1 from '../images/blogs/blog1.jpeg';
-import blog2 from '../images/blogs/blog2.svg';
-import blog3 from '../images/blogs/blog3.svg';
-import blog4 from '../images/blogs/blog4.svg';
 
 const blogs = [
   {
@@ -11,30 +8,6 @@ const blogs = [
     month: 'October',
     image: blog1,
     link: 'https://medium.com/@jossanymoura/a-little-about-me-and-how-i-discovered-development-167e5bcbc713',
-  },
-  {
-    id: 2,
-    title: 'How to create a Blog',
-    date: '01',
-    month: 'March',
-    image: blog2,
-    link: 'https://www.google.com/',
-  },
-  {
-    id: 3,
-    title: 'How to create a Blog',
-    date: '01',
-    month: 'March',
-    image: blog3,
-    link: 'https://www.google.com/',
-  },
-  {
-    id: 4,
-    title: 'How to create a Blog',
-    date: '01',
-    month: 'March',
-    image: blog4,
-    link: 'https://www.google.com/',
   },
 ];
 
--- src/data/portfolio.jsx
+++ src/data/portfolio.jsx
@@ -1,8 +1,4 @@
 import portfolioImg from '../images/portImages/responsive-portfolio.png';
-import css2 from '../images/portImages/ms.png';
-import react1 from '../images/portImages/react_styled.png';
-import img2 from '../images/portImages/maya-4.jpg';
-import img3 from '../images/portImages/maya-3.jpg';
 
 const portfolio = [
   {
@@ -12,60 +8,6 @@ const portfolio = [
     link1: 'https://github.com/mjossany/portfolio',
     title: 'Responsive Resume',
     text: 'Created with only React and Styled Components',
-  },
-  {
-    id: 2,
-    category: 'Python',
-    image: img3,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'How To Animate In Maya',
-    text: 'Number one Animation Application',
-  },
-  {
-    id: 3,
-    category: 'Javascript',
-    image: img3,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'How To Animate In Maya',
-    text: 'Number one Animation Application',
-  },
-  {
-    id: 4,
-    category: 'Animation',
-    image: img2,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'How To Use Blender',
-    text: 'Free Animation Software',
-  },
-  {
-    id: 5,
-    category: 'Animation',
-    image: img2,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'How To Use Blender',
-    text: 'Free Animation Software',
-  },
-  {
-    id: 6,
-    category: 'React JS',
-    image: react1,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'Responsive Portfolio Website',
-    text: 'Created using different technologies such as Material UI, Styled-Components and more...',
-  },
-  {
-    id: 7,
-    category: 'CSS',
-    image: css2,
-    link1: 'https://www.google.com',
-    link2: 'https://www.google.com',
-    title: 'Microsoft Website Clone',
-    text: 'Created using HTML and CSS',
   },
 ];
 
