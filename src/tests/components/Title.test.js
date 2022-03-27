6d512c1a643a8bf2f2e3ca92f73117b3c4cb3e8f
Jossany Moura <mjossany@gmail.com>
2022-03-27 18:51:57 -0300

    Completes Title component tests

--- src/tests/components/Title.test.js
+++ src/tests/components/Title.test.js
@@ -5,7 +5,14 @@ import Title from '../../components/Title/Title';
 describe('Title component tests', () => {
   it('Tests component renderization', () => {
     render(<Title title="Oi" span="Teste" />);
-
     expect(screen.getByText('Oi')).toBeInTheDocument();
+  });
+
+  it('Tests if the Title component render the right content', () => {
+    render(<Title title="Testing" span="Shadow" />);
+
+    const titleProp = screen.getByRole('heading', { level: 2 });
+    expect(titleProp).toHaveTextContent('Testing');
+    expect(screen.getByText('Testing')).toBeInTheDocument();
   });
 });
