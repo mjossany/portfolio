0c48949d511e8f27d845a570361e44cdd624406e
Jossany Moura <mjossany@gmail.com>
2022-03-27 21:08:35 -0300

    Install mq-polyfill and create the mock to resize the screen for the tests

--- src/tests/components/Sidebar.test.js
+++ src/tests/components/Sidebar.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '../../test-utils/testing-library-utils';
+import { render, screen } from '../../test-utils/testing-library-utils';
 import Sidebar from '../../components/Sidebar/Sidebar';
 
 describe('Sidebar component tests', () => {
   it('Tests if the Sidebar renders all buttons', () => {
     render(<Sidebar />);
+    const linksText = ['Home', 'About', 'Resume', 'Portfolio', 'Blogs', 'Contact'];
+    const links = screen.getAllByRole('link');
+    links.forEach((btn, index) => {
+      expect(btn).toHaveTextContent(linksText[index]);
+    });
+  });
+
+  it('Tests if an image is rendered on the Sidebar', () => {
+    render(<Sidebar />);
+    const avatarImage = screen.getByRole('img');
+    expect(avatarImage).toBeInTheDocument();
   });
 });
