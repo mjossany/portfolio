59581d40b55be0a42d9854324390c5932e60b270
Jossany Moura <mjossany@gmail.com>
2022-03-29 19:13:21 -0300

    .

--- src/tests/components/LightModeSwitch.test.js
+++ src/tests/components/LightModeSwitch.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render, screen } from '../../test-utils/testing-library-utils';
 import LightModeSwitch from '../../components/LightModeSwitch/LightModeSwitch';
+// import usePortfolio from '../../hooks/usePortfolio';
 
 describe('LightModeSwitch component tests', () => {
   it('Tests if the LightModeSwitch is rendered', () => {
     render(<LightModeSwitch />);
     const componentSwitch = screen.getByRole('checkbox');
     expect(componentSwitch).toBeInTheDocument();
+    userEvent.click(componentSwitch);
+    userEvent.click(componentSwitch);
   });
 });
